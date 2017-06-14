b0cc4c8de1c7083a7a7a8380c635eb1b20716e44
Edward <edward_song@yeah.net>
2017-06-14 15:45:01 +0800

    release v1.0.2.0

---
+++ UploadSFCS/IniFile.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace UploadSFCS
+{
+    /// <summary>
+    /// INI文件的操作类
+    /// </summary>
+    public  class IniFile
+    {
+
+        /// <summary>
+        /// ini file path value
+        /// </summary>
+        public  static  string iniFilePathValue;
+
+        /// <summary>
+        /// set/get ini file path value
+        /// </summary>
+        public static string IniFilePath
+        {
+            get
+            {
+                return iniFilePathValue;
+            }
+
+            set
+            {
+                iniFilePathValue = value;
+            }
+        }
+
+
+   
+
+#region declare Read_Write ini file API
+
+    [DllImport ("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath); 
+
+    [DllImport ("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string defVal, StringBuilder retVal, int size, string filePath); 
+	#endregion
+
+#region Write INI file
+        /// <summary>
+        /// 写INI文件
+        /// </summary>
+       /// <param name="section">段落</param>
+       /// <param name="key">键</param>
+       /// <param name="iValue">值</param>
+       /// <param name="filePath">ini文件地址</param>
+		
+        public static  void IniWriteValue(string section,string key,string iValue,string filePath)
+        {
+            WritePrivateProfileString (section,key,iValue,filePath);
+        }
+
+        /// <summary>
+        ///  Write INI File
+        /// </summary>
+        /// <param name="section">段落</param>
+        /// <param name="key">键</param>
+        /// <param name="iValue">值</param>
+        public static void IniWriteValue(string section, string key, string iValue)
+        {
+            if (!string.IsNullOrEmpty(iniFilePathValue))
+            {
+
+                WritePrivateProfileString(section, key, iValue, iniFilePathValue);
+            }
+            else
+            {
+                throw (new ArgumentNullException("Write INI File", "INI file path can't be null or empty,please assign INI file path again!"));
+            }
+        }
+	#endregion
+
+#region Read INI file
+
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="section">段落</param>
+        /// <param name="key">键</param>
+        /// <param name="filePath">ini文件的地址</param>      
+        public static  string IniReadValue(string section, string key,string filePath) 
+        { 
+           StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(section, key, "", temp, 255, filePath ); 
+            return temp.ToString();
+        }
+
+
+
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="section">段落</param>
+        /// <param name="key">键</param>
+        public static string IniReadValue(string section, string key)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            if (!string.IsNullOrEmpty(iniFilePathValue))
+            {
+                int i = GetPrivateProfileString(section, key, "", temp, 255, iniFilePathValue);
+            }
+            else
+            {
+                throw (new ArgumentNullException("Read INI file", "INI file path can't be null or empty,please assign INI file path again!"));
+            }
+            return temp.ToString();
+        }
+
+	#endregion
+
+    #region CreateIniFile
+
+        /// <summary>
+        /// create ini file
+        /// </summary>
+        /// <param name="filePath">ini file path</param>
+        public static void  CreateIniFile(string filePath)
+        {
+
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                //check file exits
+
+                if (!File.Exists(filePath))
+                {
+                    //file is not exits,create it
+                    try
+                    {
+                        FileStream fs = File.Create(filePath);
+                        fs.Close();
+                    }
+                    catch (Exception e)
+                    {                        
+                        throw e;
+                    }                  
+
+                }
+            }
+
+            else  //file path is null or empty
+            {
+                throw (new ArgumentNullException("Read INI file", "INI file path can't be null or empty,please assign INI file path again!"));
+               
+            }
+       
+
+        }
+
+        /// <summary>
+        /// create ini file
+        /// </summary>
+        public static void CreateIniFile()
+        {
+
+            if (!string.IsNullOrEmpty(iniFilePathValue))
+            {
+                //check file exits
+
+                if (!File.Exists(iniFilePathValue ))
+                {
+                    //file is not exits,create it
+                    try
+                    {
+                        FileStream fs = File.Create(iniFilePathValue );
+                        fs.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        throw e;
+                    }
+
+                }
+            }
+
+            else  //file path is null or empty
+            {
+                throw (new ArgumentNullException("Read INI file", "INI file path can't be null or empty,please assign INI file path again!"));
+
+            }
+
+
+        }
+
+        #endregion
+
+
+
+    }    
+
+}
---
+++ UploadSFCS/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+using System.Drawing;
+
+
+
+namespace UploadSFCS
+{
+    class Program
+    {
+        #region 參數
+
+        static string iniFilePath = System.Windows.Forms.Application.StartupPath + @"\UploadSFCS.ini";
+        static string webSite = @"http://10.62.201.35/Tester.WebService/WebService.asmx";        
+        static  PCB_Web.WebService ws = new PCB_Web.WebService();
+     
+        //SFCS
+        static string PPID = string.Empty;
+        static string LINE = string.Empty;
+        static string STAGE = "TA";
+        static string OPID = "K12345678";
+        static bool PassFlag = true;
+        static string[] trndata = new string[1]; //上拋SFCS的附件信息,=ERRORCODE
+        static string FIXTUREID = string.Empty;
+        
+
+        #endregion
+        static void Main(string[] args)
+        {
+
+            //check ini 
+            InitIniFile();
+            displayMsg();
+            //check web
+            if (!checkWebSite(webSite))
+            {
+                //error
+
+            }
+
+
+            Console.WriteLine(webSite);
+
+            if (args.Length == 7)
+            {
+                PPID = args[0].ToUpper().Trim();
+                LINE = args[1].ToUpper().Trim();
+                STAGE = args[2].Trim().ToUpper();
+                OPID = args[3].Trim().ToUpper();
+                if (args[4].ToUpper().Trim() == "PASS")
+                {
+                    PassFlag = true;
+                    trndata[0] = args[0].ToUpper().Trim();
+                }
+                if (args[4].ToUpper().Trim() == "FAIL")
+                {
+                    PassFlag = false;
+                    trndata[0] = args[5].ToUpper().Trim();
+                }
+                
+                FIXTUREID = args[6].ToUpper().Trim();
+
+                string result = string.Empty;
+                result = ws.UploadFixtureID(PPID, STAGE, FIXTUREID);
+                result  = ws.Complete (PPID,LINE,STAGE,STAGE,OPID,PassFlag,trndata );
+                if (result == "OK")
+                {
+                    SaveLog(PPID + "->upload OK");
+                    Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss") + " " + PPID + "->upload OK");
+                }
+                else
+                {
+                    SaveLog(PPID + "->upload NG," + result);
+                    Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss") + " " + PPID + "->upload NG," + result);
+                }
+            }
+            else 
+            {
+               // displayMsg();
+            }
+
+
+
+         // Console.ReadKey();
+
+
+        }
+
+
+        private static  void InitIniFile()
+        {
+            // check file exits
+            if (!File.Exists(iniFilePath))
+            {
+                IniFile.CreateIniFile(iniFilePath);
+                IniFile.IniWriteValue("SysConfig", "WebSite", "http://10.62.201.35/Tester.WebService/WebService.asmx", iniFilePath);
+            }
+            else
+            {
+                string tempWeb = IniFile.IniReadValue("SysConfig", "WebSite", iniFilePath);
+                if (!string.IsNullOrEmpty(tempWeb))
+                    webSite = tempWeb;
+            }
+
+        }
+
+        private static  bool checkWebSite(string website)
+        {
+            ws.Url = webSite;
+            try
+            {
+                ws.Discover();
+            }
+            catch (Exception ex )
+            {
+                Console.WriteLine( DateTime.Now.ToString ("yyyyMMddHHmmss") + " Connect WebService error.");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss") + " pls check the websit or network.");                
+                return false;
+            }            
+            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss") + " Connect WebService ok.");
+            return true;
+        }
+
+        public static void SaveLog(string logcontent)
+        {
+            string appFolder = Application.StartupPath + @"\UploadSFCS";
+            string filePath = appFolder + @"\" + @DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            string log = DateTime.Now.ToString("yyyyMMddHHmmss") + " " + logcontent + "\r\n";
+            //check folder & file
+            if (!Directory.Exists(appFolder))
+                Directory.CreateDirectory(appFolder);
+            if (!File.Exists(filePath))
+            {
+                FileStream fs = File.Create(filePath);
+                fs.Close();
+            }
+            else
+            {
+                File.AppendAllText(@filePath, @log);
+            }
+
+        }
+
+        public static void displayMsg()
+        {
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*    UploadSFCS use command via webservice                               *");
+            Console.WriteLine("*    Ver:1.0.0.0, Author:WCD ATE Edward_song@yeah.net                    *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*    Command List:                                                       *");
+            Console.WriteLine("*    UploadSFCS.exe PPID  Line Stage OPID PassFlag ErrorCode FixtureID   *");
+            Console.WriteLine("*    PPID:the MB ppid or sn,eg:CN12345678901234567890                    *");
+            Console.WriteLine("*    Line:pcb line name,eg:AP2                                           *");
+            Console.WriteLine("*    Stage:stage code,eg:TA                                              *");
+            Console.WriteLine("*    OPID:OP id,eg:D1203ABJ0                                             *");
+            Console.WriteLine("*    PassFlage:the MB test result,pass or fail,eg:pass                   *");
+            Console.WriteLine("*    ErrorCode:the MB test result code,eg:AT01 or 0000                   *");
+            Console.WriteLine("*    FixtureID:the fixture id,eg:15203-1-L                               *");
+            Console.WriteLine("**************************************************************************");
+        }
+    }
+}
